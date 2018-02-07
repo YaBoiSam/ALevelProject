0d193514af92542021761986360e9d3ae8620a8c
SamHallSchool <wamble7.sam@gmail.com>
2018-02-07 10:28:36 +0000

    First Index.php

---
+++ WorkInProgress/login.php
@@ -0,0 +1,7 @@
+<html>
+    <title>Login</title>
+    <head>
+        <link rel="shortcut icon" type="image/x-icon" href="http://fabrizio.me.uk/wp-content/uploads/2010/07/11949848541326072700padlock_aj_ashton_01.svg_.hi1_.png" />
+        <link rel="stylesheet" href="background.css">
+    </head>
+</html>
